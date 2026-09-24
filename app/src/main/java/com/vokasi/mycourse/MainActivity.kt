2e197ecab833396a641417b6cbd669173400d65f
klarissaslbn <klarissasilabann@gmail.com>
2026-09-24 09:25:41 +0700

    initial commit tab layout

--- app/src/main/java/com/vokasi/mycourse/MainActivity.kt
+++ app/src/main/java/com/vokasi/mycourse/MainActivity.kt
@@ -1,26 +1,67 @@
 package com.vokasi.mycourse
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
+import androidx.viewpager2.widget.ViewPager2
+
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+
+        ViewCompat.setOnApplyWindowInsetsListener(
+            findViewById(R.id.tab_layout)
+        ) { v, insets ->
+
+            val systemBars =
+                insets.getInsets(WindowInsetsCompat.Type.systemBars())
+
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                systemBars.bottom
+            )
+
             insets
         }
+
+        // Menghubungkan SectionsPagerAdapter dengan ViewPager2
+        val sectionsPagerAdapter = SectionsPagerAdapter(this)
+
+        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
+
+        viewPager.adapter = sectionsPagerAdapter
+
+        // Menghubungkan TabLayout dengan ViewPager2
+        val tabs: TabLayout = findViewById(R.id.tab_layout)
+
+        val tabTitles = arrayOf(
+            "Home",
+            "Materi",
+            "Quiz"
+        )
+
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = tabTitles[position]
+        }.attach()
     }
+
+    // OPTIONS MENU
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_options, menu)
